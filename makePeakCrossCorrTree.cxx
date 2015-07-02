9fc78a989086c0ca9ae07ec8f320ddfdf60a5b23
strutt <strutt@d90a992a-4ffb-0310-9b0e-ce574b03d632>
2015-07-02 15:47:23 +0000

    First working version of makePeakCrossCorrTree?
    git-svn-id: https://delos.mps.ohio-state.edu/anitaGround/strutt/anita3Analysis/antennaPositionCalib@3389 d90a992a-4ffb-0310-9b0e-ce574b03d632

--- makePeakCrossCorrTree.cxx
+++ makePeakCrossCorrTree.cxx
@@ -1,5 +1,7 @@
 #include <UsefulAnitaEvent.h>
 #include <RawAnitaHeader.h>
+#include <UsefulAdu5Pat.h>
+#include <FFTtools.h>
 
 #include <TFile.h>
 #include <TTree.h>
@@ -16,7 +18,6 @@
 #include <ProgressBar.h>
 #include <FancyTTreeInterpolator.h>
 
-
 int main(int argc, char* argv[]){
 
   if(argc>1){
@@ -26,6 +27,7 @@ int main(int argc, char* argv[]){
   const Int_t startRun = 331;
   const Int_t endRun = 355;
 
+  // Thanks steph
   const Double_t sourceLat = - (79 + (27.93728/60));
   const Double_t sourceLon = -(112 + (6.74974/60));
   const Double_t sourceAlt = 1813.42;
@@ -36,13 +38,14 @@ int main(int argc, char* argv[]){
   TChain* eventChain = new TChain("eventTree");
   TChain* gpsChain = new TChain("adu5PatTree");
   for(Int_t run=startRun; run<endRun; run++){
-    TString eventFileName = TString::Format("root/run%d/eventFile%d.root", run, run);
+    TString eventFileName = TString::Format("~/UCL/ANITA/flight1415/root/run%d/eventFile%d.root", run, run);
     eventChain->Add(eventFileName);
 
-    TString rawHeaderFileName = TString::Format("root/run%d/eventHeadFile%d.root", run, run);
+    TString rawHeaderFileName = TString::Format("~/UCL/ANITA/flight1415/root/run%d/eventHeadFile%d.root", 
+						run, run);
     headChain->Add(rawHeaderFileName);
 
-    TString gpsFileName = TString::Format("root/run%d/gpsFile%d.root", run, run);
+    TString gpsFileName = TString::Format("~/UCL/ANITA/flight1415/root/run%d/gpsFile%d.root", run, run);
     gpsChain->Add(gpsFileName);
   }
 
@@ -65,30 +68,31 @@ int main(int argc, char* argv[]){
   gpsInterp.get("pat->latitude")->SetName("grLat");
   gpsInterp.get("pat->longitude")->SetName("grLong");
   
-  TGraph* gr = gpsInterp.get("pat->heading");
-  gpsInterp.get("pat->heading")->Write();
-  gpsInterp.get("pat->altitude")->Write();
-  gpsInterp.get("pat->latitude")->Write();
-  gpsInterp.get("pat->longitude")->Write();
+  // TGraph* gr = gpsInterp.get("pat->heading");
+  // gpsInterp.get("pat->heading")->Write();
+  // gpsInterp.get("pat->altitude")->Write();
+  // gpsInterp.get("pat->latitude")->Write();
+  // gpsInterp.get("pat->longitude")->Write();
 
-  TTree* antPosTree = new TTree("antPosTree","antPosTree");
+  TTree* antTree = new TTree("antTree","antTree");
   UInt_t eventNumber = 0;
   Double_t peakCorr = 0;
   Double_t peakTime = 0;
+  antTree->Branch("eventNumber", &eventNumber);
+  antTree->Branch("peakCorr", &peakCorr);
+  antTree->Branch("peakTime", &peakTime);
 
-  Long64_t nEntries = eventTree->GetEntries();
+  Long64_t nEntries = headChain->GetEntries();
   Long64_t maxEntry = 10000;
   if(maxEntry<=0 || maxEntry > nEntries) maxEntry = nEntries;
-  std::cout << "Processing " << maxEntry << " of " << nEntries << " in file " << eventFileName << std::endl;
+  std::cout << "Processing " << maxEntry << " of " << nEntries << std::endl;
 
   Int_t ant1 = 0;
   Int_t ant2 = 18;
 
   ProgressBar p(maxEntry);
 
-  Double_t cutTimeNs = 1200; //60e3;
-
-  TH2D* hTrigTime;
+  // TH2D* hTrigTime = ;
 
   for(Long64_t entry=0; entry<maxEntry; entry++){
 
@@ -103,37 +107,33 @@ int main(int argc, char* argv[]){
       pat2.longitude = gpsInterp.interp("pat->longitude", header->realTime);
       pat2.altitude = gpsInterp.interp("pat->altitude", header->realTime);
 
-      
-      //      UsefulAdu5Pat usefulPat(pat);
       UsefulAdu5Pat usefulPat(&pat2);
       UInt_t triggerTimeNsExpected = usefulPat.getTriggerTimeNsFromSource(sourceLat, sourceLon, sourceAlt);
 
       if(TMath::Abs(Double_t(triggerTimeNsExpected) - Double_t(triggerTimeNs)) < cutTimeNs){
-
 
 	eventChain->GetEntry(entry);
       
 	UsefulAnitaEvent usefulEvent(event, WaveCalType::kDefault, header);
       
 
-	TGraph* gr1 = usefulEvent.get(ant1);
-	TGraph* gr2 = usefulEvent.get(ant2);
+	TGraph* gr1 = usefulEvent.getGraph(ant1);
+	TGraph* gr2 = usefulEvent.getGraph(ant2);
 
 	TGraph* grCorr = FFTtools::getInterpolatedCorrelationGraph(gr1, gr2, 0.01);
 
 	Double_t minX, minY;
-	RootTools::getMinMax(grCorr, minY, minX, peakCorr, peakTime);
+	RootTools::getMaxMin(grCorr, peakCorr, peakTime,minY, minX);
 
+	antTree->Fill();
 
 	delete gr1;
 	delete gr2;
 	delete grCorr;
       }
-    }    
-
-
+    }  
+    p++;
   }
-
   
   outFile->Write();
   outFile->Close();
