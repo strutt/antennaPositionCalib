06a930f96538e823fb88f3e36495528c8d59a3fd
strutt <strutt@d90a992a-4ffb-0310-9b0e-ce574b03d632>
2016-03-03 17:08:56 +0000

    First look at revisiting pitch/roll offsets
    git-svn-id: https://delos.mps.ohio-state.edu/anitaGround/strutt/anita3Analysis/antennaPositionCalib@3536 d90a992a-4ffb-0310-9b0e-ce574b03d632

---
+++ drawLookForPitchRollOffsetPlots.C
@@ -0,0 +1,99 @@
+void drawLookForPitchRollOffsetPlots(){
+
+
+
+  auto pitchRollChain = new TChain("pitchRollTree");
+  pitchRollChain->Add("lookForPitchRollOffsetsPlots/lookForPitchRollOffsetsPlots_*");
+
+  const Int_t numPitches = 11;
+  const Double_t startPitch = -2;
+  const Double_t deltaPitch = 0.4;
+  Double_t pitchOffsets[numPitches] = {0};
+  for(Int_t pitchInd=0; pitchInd < numPitches; pitchInd++){
+    pitchOffsets[pitchInd] = startPitch + deltaPitch*pitchInd;
+  }
+  
+  const Int_t numRolls = 11;
+  const Double_t startRoll = -2;
+  const Double_t deltaRoll = 0.4;
+  Double_t rollOffsets[numRolls] = {0};
+  for(Int_t rollInd=0; rollInd < numRolls; rollInd++){
+    rollOffsets[rollInd] = startRoll + deltaRoll*rollInd;
+  }  
+
+
+  // std::vector<TProfile*> profs;  
+  // for(Int_t pitchInd=0; pitchInd < numPitches; pitchInd++){
+  //   for(Int_t rollInd=0; rollInd < numRolls; rollInd++){
+
+  //     TString profName = TString::Format("hProf_%d_%d", pitchInd, rollInd);
+  //     TString profTitle = TString::Format("Pitch %2.1lf, Roll %2.1lf",
+  // 					  pitchOffsets[pitchInd], rollOffsets[rollInd]);
+
+  //     TProfile* hProf = new TProfile(profName, profTitle, 256, 0, 360);
+
+  //     // TString command = TString::Format("deltaPhiDegs[%d][%d]:zoomPhiDeg>>", pitchInd, rollInd);
+  //     // command += profName;
+  //     TString command = TString::Format("deltaThetaDegs[%d][%d]:zoomPhiDeg>>", pitchInd, rollInd);
+  //     command += profName;
+
+  //     std::cout << pitchInd << "\t" << rollInd << "\t" << command.Data() << std::endl;
+
+  //     TString cuts = TString::Format("TMath::Abs(deltaPhiDegs[%d][%d]) < 10", pitchInd, rollInd);
+  //     pitchRollChain->Draw(command, cuts, "goff");
+
+  //     profs.push_back(hProf);
+
+  //     hProf->SetFillColor(0);
+      
+  //     // TString drawOpt = "same";
+  //     // if(pitchInd==0 && rollInd==0){
+  //     // 	drawOpt = "";
+  //     // }
+  //     // hProf->Draw(drawOpt);
+  //   }
+  // }
+
+  // TCanvas* c1 = RootTools::drawArrayOfHistosPrettily((TH1D**) &profs[0], (Int_t) profs.size(), NULL, NULL);
+  // TLegend* l = c1->BuildLegend();
+    
+  // l->Draw();
+  // profs[0]->SetTitle("Pitch roll offsets");
+
+
+
+
+
+
+  TH2D* hThetaRmsVals = new TH2D("hThetaRmsVals",
+				 "RMS of #delta#theta distributions; Pitch offset (degrees); Roll offset (degrees); RMS (Degrees)",
+				 numPitches, pitchOffsets[0], pitchOffsets[numPitches-1]+deltaPitch,
+				 numRolls, rollOffsets[0], rollOffsets[numRolls-1]+deltaRoll);
+
+  ProgressBar p(numPitches*numRolls);
+  for(Int_t pitchInd=0; pitchInd < numPitches; pitchInd++){
+    for(Int_t rollInd=0; rollInd < numRolls; rollInd++){
+
+      TString histName = TString::Format("h_%d_%d", pitchInd, rollInd);
+      TString histTitle = TString::Format("Pitch %2.1lf, Roll %2.1lf",
+					  pitchOffsets[pitchInd], rollOffsets[rollInd]);
+
+      TH1D* h = new TH1D(histName, histTitle, 200, -10, 10);
+
+      // TString command = TString::Format("deltaPhiDegs[%d][%d]:zoomPhiDeg>>", pitchInd, rollInd);
+      // command += histName;
+      TString command = TString::Format("deltaThetaDegs[%d][%d]>>", pitchInd, rollInd);
+      command += histName;
+
+      TString cuts = TString::Format("TMath::Abs(deltaPhiDegs[%d][%d]) < 10", pitchInd, rollInd);
+      pitchRollChain->Draw(command, cuts, "goff");
+
+
+      hThetaRmsVals->SetBinContent(pitchInd+1, rollInd+1, h->GetRMS());
+      p++;
+    }
+  }
+  hThetaRmsVals->Draw("colz");
+  
+
+}
---
+++ lookForPitchRollOffsets.cxx
@@ -0,0 +1,195 @@
+// -*- C++ -*-.
+/*****************************************************************************************************************
+ Author: Ben Strutt
+ Email: b.strutt.12@ucl.ac.uk
+
+ Description:
+             Program to look at photogrammetry reconstructed positions in januaryTests/photogrammetryPosition
+             And apply varying pitch/roll offsets and look at mean and RMS.
+*************************************************************************************************************** */
+
+#include "TFile.h"
+#include "TChain.h"
+#include "TGraph.h"
+#include "TH1D.h"
+#include "TH2D.h"
+#include "TCanvas.h"
+#include "TLegend.h"
+#include "TProfile2D.h"
+#include "THnSparse.h"
+
+#include "RawAnitaHeader.h"
+#include "UsefulAdu5Pat.h"
+#include "UsefulAnitaEvent.h"
+#include "CalibratedAnitaEvent.h"
+#include "AnitaEventCalibrator.h"
+
+#include "ProgressBar.h"
+#include "CrossCorrelator.h"
+#include "OutputConvention.h"
+
+
+// // Copied from UsefulAdu5Pat.h
+// // I need to remove the const from this namespace...
+// namespace AnitaStaticAdu5Offsets{
+//   const Double_t heading = 0;
+//   const Double_t pitch = 0;
+//   const Double_t roll = 0;
+// }
+
+
+int main(int argc, char *argv[])
+{
+
+  if(!(argc==3 || argc==2)){
+    std::cerr << "Usage 1: " << argv[0] << " [run]" << std::endl;
+    std::cerr << "Usage 2: " << argv[0] << " [firstRun] [lastRun]" << std::endl;
+    return 1;
+  }
+  
+  std::cout << argv[0] << "\t" << argv[1];
+  if(argc==3){std::cout << "\t" << argv[2];}
+  std::cout << std::endl;
+  const Int_t firstRun = atoi(argv[1]);
+  const Int_t lastRun = firstRun; //argc==3 ? atoi(argv[2]) : firstRun;
+
+
+  TChain* gpsChain = new TChain("adu5PatTree");
+  TChain* angResChain = new TChain("angResTree");
+  for(Int_t run=firstRun; run<=lastRun; run++){
+    TString fileName = TString::Format("~/UCL/ANITA/flight1415/root/run%d/gpsEvent%d.root", run, run);
+    // fileName = TString::Format("~/UCL/ANITA/flight1415/root/run%d/gpsFile%d.root", run, run);    
+    gpsChain->Add(fileName);
+
+    // Will wildcard matching work
+    fileName = TString::Format("januaryTests/photogrammetryNumbers/generateAngularResolutionTreePlots_%d*.root", run);
+    angResChain->Add(fileName);
+  }
+  Adu5Pat* pat = NULL;
+  gpsChain->SetBranchAddress("pat", &pat);
+  gpsChain->BuildIndex("eventNumber");  
+  UInt_t eventNumberGps = 0;
+  gpsChain->SetBranchAddress("eventNumber", &eventNumberGps);
+
+
+  
+  UInt_t eventNumber = 0;
+  angResChain->SetBranchAddress("eventNumber", &eventNumber);
+  Double_t zoomPhiDeg = 0;
+  angResChain->SetBranchAddress("zoomPhiDeg", &zoomPhiDeg);
+  Double_t zoomThetaDeg = 0;
+  angResChain->SetBranchAddress("zoomThetaDeg", &zoomThetaDeg);
+
+
+
+
+
+
+
+
+  OutputConvention oc(argc, argv);
+  TString outFileName = oc.getOutputFileName();
+  TFile* outFile = new TFile(outFileName, "recreate");
+  if(outFile->IsZombie()){
+    std::cerr << "Error! Unable to open output file " << outFileName.Data() << std::endl;
+    return 1;
+  }
+
+  TTree* pitchRollTree = new TTree("pitchRollTree", "pitchRollTree");
+
+  const Int_t numPitches = 11;
+  const Double_t startPitch = -2;
+  const Double_t deltaPitch = 0.4;
+  Double_t pitchOffsets[numPitches] = {0};
+  for(Int_t pitchInd=0; pitchInd < numPitches; pitchInd++){
+    pitchOffsets[pitchInd] = startPitch + deltaPitch*pitchInd;
+  }
+  pitchRollTree->Branch(TString::Format("pitchOffsets[%d]", numPitches), pitchOffsets);
+
+  
+  const Int_t numRolls = 11;
+  const Double_t startRoll = -2;
+  const Double_t deltaRoll = 0.4;
+  Double_t rollOffsets[numRolls] = {0};
+  for(Int_t rollInd=0; rollInd < numRolls; rollInd++){
+    rollOffsets[rollInd] = startRoll + deltaRoll*rollInd;
+  }  
+  pitchRollTree->Branch(TString::Format("rollOffsets[%d]", numRolls), rollOffsets);  
+
+
+  Double_t deltaPhiDegs[numPitches][numRolls];
+  pitchRollTree->Branch("deltaPhiDegs",deltaPhiDegs,
+			TString::Format("deltaPhiDegs[%d][%d]/D", numPitches, numRolls));
+  Double_t deltaThetaDegs[numPitches][numRolls];
+  pitchRollTree->Branch("deltaThetaDegs",deltaThetaDegs,
+			TString::Format("deltaThetaDegs[%d][%d]/D", numPitches, numRolls));
+
+
+  Double_t thetaExpected[numPitches][numRolls];
+  pitchRollTree->Branch("thetaExpected",thetaExpected,
+			TString::Format("thetaExpected[%d][%d]/D", numPitches, numRolls));
+  Double_t phiExpected[numPitches][numRolls];
+  pitchRollTree->Branch("phiExpected",phiExpected,
+			TString::Format("phiExpected[%d][%d]/D", numPitches, numRolls));
+  UInt_t eventNumberPitchRoll = 0;
+  pitchRollTree->Branch("eventNumber", &eventNumberPitchRoll);
+  Double_t zoomPhiDegPitchRoll = 0;
+  pitchRollTree->Branch("zoomPhiDeg", &zoomPhiDegPitchRoll);
+  Double_t zoomThetaDegPitchRoll = 0;
+  pitchRollTree->Branch("zoomThetaDeg", &zoomThetaDegPitchRoll);
+  
+  Long64_t nEntries = angResChain->GetEntries();
+  Long64_t maxEntry = 0; //5000;
+  Long64_t startEntry = 0;
+  if(maxEntry<=0 || maxEntry > nEntries) maxEntry = nEntries;
+  std::cout << "Processing " << maxEntry << " of " << nEntries << " entries." << std::endl;
+  ProgressBar p(maxEntry-startEntry);
+  for(Long64_t entry = startEntry; entry < maxEntry; entry++){
+    angResChain->GetEntry(entry);
+    gpsChain->GetEntryWithIndex(eventNumber);
+
+    // std::cout << eventNumber << "\t" << eventNumberGps << std::endl;
+    if(eventNumber != eventNumberGps){
+      std::cerr << "Warning! Unable to find eventNumber " << eventNumber << " in gpsChain!" << std::endl;
+      continue;
+    }
+
+    UsefulAdu5Pat usefulPat(pat);
+
+    eventNumberPitchRoll = eventNumber;
+    zoomThetaDegPitchRoll = zoomThetaDeg;
+    zoomPhiDegPitchRoll = zoomPhiDeg;    
+    
+    for(Int_t pitchInd=0; pitchInd < numPitches; pitchInd++){
+      usefulPat.pitch = pitchOffsets[pitchInd];
+      for(Int_t rollInd=0; rollInd < numRolls; rollInd++){
+	usefulPat.roll = rollOffsets[rollInd];
+	
+	usefulPat.getThetaAndPhiWaveWaisDivide(thetaExpected[pitchInd][rollInd], phiExpected[pitchInd][rollInd]);
+
+	// My silly convention that I've stuck to so far...
+	phiExpected[pitchInd][rollInd]*=TMath::RadToDeg();
+	thetaExpected[pitchInd][rollInd]*=-1*TMath::RadToDeg();
+
+	
+	deltaPhiDegs[pitchInd][rollInd] = RootTools::getDeltaAngleDeg(phiExpected[pitchInd][rollInd], zoomPhiDeg);
+	deltaThetaDegs[pitchInd][rollInd] = RootTools::getDeltaAngleDeg(thetaExpected[pitchInd][rollInd], zoomThetaDeg);
+
+	// std::cout << usefulPat.pitch << "\t" << usefulPat.roll << "\t" << thetaExpected[pitchInd][rollInd] << "\t" << phiExpected[pitchInd][rollInd] << "\t" << deltaPhiDegs[pitchInd][rollInd] << "\t" << deltaThetaDegs[pitchInd][rollInd] << std::endl;
+
+
+	
+	
+      }
+    }
+
+    pitchRollTree->Fill();
+    
+    p++;
+  }
+
+  outFile->Write();
+  outFile->Close();
+  
+  return 0;
+}
