55c445cc0c7165fe4d6780c60211d3b4c855b249
strutt <strutt@d90a992a-4ffb-0310-9b0e-ce574b03d632>
2016-03-03 17:09:15 +0000

    Making best fit vs measured phi plot of 0.1 degree pitch/roll steps
    git-svn-id: https://delos.mps.ohio-state.edu/anitaGround/strutt/anita3Analysis/antennaPositionCalib@3539 d90a992a-4ffb-0310-9b0e-ce574b03d632

--- drawLookForPitchRollOffsetPlots.C
+++ drawLookForPitchRollOffsetPlots.C
@@ -39,49 +39,49 @@ void drawLookForPitchRollOffsetPlots(){
   }  
 
 
-  // std::vector<TProfile*> profs;  
-  // for(Int_t pitchInd=0; pitchInd < numPitches; pitchInd++){
-  //   for(Int_t rollInd=0; rollInd < numRolls; rollInd++){
+  std::vector<TProfile*> profs;  
+  for(Int_t pitchInd=0; pitchInd < numPitches; pitchInd++){
+    for(Int_t rollInd=0; rollInd < numRolls; rollInd++){
 
-  //     if(!((pitchInd==5||pitchInd==6) && rollInd==5)){
-  // 	continue;
-  //     }
+      if(!((pitchInd==4 || pitchInd==1) && rollInd==5)){
+  	continue;
+      }
       
-  //     TString profName = TString::Format("hProf_%d_%d", pitchInd, rollInd);
-  //     TString profTitle = TString::Format("Pitch %2.1lf, Roll %2.1lf",
-  // 					  pitchOffsets[pitchInd], rollOffsets[rollInd]);
+      TString profName = TString::Format("hProf_%d_%d", pitchInd, rollInd);
+      TString profTitle = TString::Format("Pitch %2.1lf, Roll %2.1lf",
+  					  pitchOffsets[pitchInd], rollOffsets[rollInd]);
 
-  //     TProfile* hProf = new TProfile(profName, profTitle, 256, 0, 360);
+      TProfile* hProf = new TProfile(profName, profTitle, 256, 0, 360);
 
-  //     // TString command = TString::Format("deltaPhiDegs[%d][%d]:zoomPhiDeg>>", pitchInd, rollInd);
-  //     // command += profName;
-  //     TString command = TString::Format("deltaThetaDegs[%d][%d]:zoomPhiDeg>>", pitchInd, rollInd);
-  //     command += profName;
+      // TString command = TString::Format("deltaPhiDegs[%d][%d]:zoomPhiDeg>>", pitchInd, rollInd);
+      // command += profName;
+      TString command = TString::Format("deltaThetaDegs[%d][%d]:zoomPhiDeg>>", pitchInd, rollInd);
+      command += profName;
 
-  //     std::cout << pitchInd << "\t" << rollInd << "\t" << command.Data() << std::endl;
+      std::cout << pitchInd << "\t" << rollInd << "\t" << command.Data() << std::endl;
 
-  //     TString cuts = TString::Format("TMath::Abs(deltaPhiDegs[%d][%d]) < 10", pitchInd, rollInd);
-  //     pitchRollChain->Draw(command, cuts, "goff");
+      TString cuts = TString::Format("TMath::Abs(deltaPhiDegs[%d][%d]) < 10", pitchInd, rollInd);
+      pitchRollChain->Draw(command, cuts, "goff");
 
-  //     profs.push_back(hProf);
+      profs.push_back(hProf);
 
-  //     hProf->SetFillColor(0);
+      hProf->SetFillColor(0);
       
-  //     // TString drawOpt = "same";
-  //     // if(pitchInd==0 && rollInd==0){
-  //     // 	drawOpt = "";
-  //     // }
-  //     // hProf->Draw(drawOpt);
-  //   }
-  // }
+      // TString drawOpt = "same";
+      // if(pitchInd==0 && rollInd==0){
+      // 	drawOpt = "";
+      // }
+      // hProf->Draw(drawOpt);
+    }
+  }
 
-  // TCanvas* c1 = RootTools::drawArrayOfHistosPrettily((TH1D**) &profs[0], (Int_t) profs.size(), NULL, NULL);
-  // TLegend* l = c1->BuildLegend();
+  TCanvas* c1 = RootTools::drawArrayOfHistosPrettily((TH1D**) &profs[0], (Int_t) profs.size(), NULL, NULL);
+  TLegend* l = c1->BuildLegend();
     
-  // l->Draw();
-  // profs[0]->SetTitle("Pitch roll offsets");
+  l->Draw();
+  profs[0]->SetTitle("Pitch roll offsets");
 
-  // return ;
+  return ;
 
 
 
