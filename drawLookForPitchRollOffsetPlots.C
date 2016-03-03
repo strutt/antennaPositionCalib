7eaab25614dcf073babd9f793d4e1cb27e79e58e
strutt <strutt@d90a992a-4ffb-0310-9b0e-ce574b03d632>
2016-03-03 17:09:20 +0000

    macro to draw the output of testing zeroing channel 16BH in reconstruction
    git-svn-id: https://delos.mps.ohio-state.edu/anitaGround/strutt/anita3Analysis/antennaPositionCalib@3540 d90a992a-4ffb-0310-9b0e-ce574b03d632

--- drawLookForPitchRollOffsetPlots.C
+++ drawLookForPitchRollOffsetPlots.C
@@ -3,47 +3,48 @@ void drawLookForPitchRollOffsetPlots(){
 
 
   auto pitchRollChain = new TChain("pitchRollTree");
-  pitchRollChain->Add("lookForPitchRollOffsetsPlots/lookForPitchRollOffsetsPlots_*");
-
-  // const Int_t numPitches = 11;
-  // const Double_t startPitch = -2;
-  // const Double_t deltaPitch = 0.4;
-  // Double_t pitchOffsets[numPitches] = {0};
-  // for(Int_t pitchInd=0; pitchInd < numPitches; pitchInd++){
-  //   pitchOffsets[pitchInd] = startPitch + deltaPitch*pitchInd;
-  // }
-  
-  // const Int_t numRolls = 11;
-  // const Double_t startRoll = -2;
-  // const Double_t deltaRoll = 0.4;
-  // Double_t rollOffsets[numRolls] = {0};
-  // for(Int_t rollInd=0; rollInd < numRolls; rollInd++){
-  //   rollOffsets[rollInd] = startRoll + deltaRoll*rollInd;
-  // }  
-
+  // pitchRollChain->Add("lookForPitchRollOffsetsPlots/lookForPitchRollOffsetsPlots_*");
+  pitchRollChain->Add("lookForPitchRollOffsetsPlotsCoarse_2016_01_28/lookForPitchRollOffsetsPlots_*");
 
   const Int_t numPitches = 11;
-  const Double_t startPitch = -0.1; //-2;
-  const Double_t deltaPitch = 0.1; //0.4;
+  const Double_t startPitch = -2;
+  const Double_t deltaPitch = 0.4;
   Double_t pitchOffsets[numPitches] = {0};
   for(Int_t pitchInd=0; pitchInd < numPitches; pitchInd++){
     pitchOffsets[pitchInd] = startPitch + deltaPitch*pitchInd;
   }
   
   const Int_t numRolls = 11;
-  const Double_t startRoll = -0.5; //-2;
-  const Double_t deltaRoll = 0.1; //0.4;
+  const Double_t startRoll = -2;
+  const Double_t deltaRoll = 0.4;
   Double_t rollOffsets[numRolls] = {0};
   for(Int_t rollInd=0; rollInd < numRolls; rollInd++){
     rollOffsets[rollInd] = startRoll + deltaRoll*rollInd;
   }  
 
 
+  // const Int_t numPitches = 11;
+  // const Double_t startPitch = -0.1; //-2;
+  // const Double_t deltaPitch = 0.1; //0.4;
+  // Double_t pitchOffsets[numPitches] = {0};
+  // for(Int_t pitchInd=0; pitchInd < numPitches; pitchInd++){
+  //   pitchOffsets[pitchInd] = startPitch + deltaPitch*pitchInd;
+  // }
+  
+  // const Int_t numRolls = 11;
+  // const Double_t startRoll = -0.5; //-2;
+  // const Double_t deltaRoll = 0.1; //0.4;
+  // Double_t rollOffsets[numRolls] = {0};
+  // for(Int_t rollInd=0; rollInd < numRolls; rollInd++){
+  //   rollOffsets[rollInd] = startRoll + deltaRoll*rollInd;
+  // }  
+
+
   std::vector<TProfile*> profs;  
   for(Int_t pitchInd=0; pitchInd < numPitches; pitchInd++){
     for(Int_t rollInd=0; rollInd < numRolls; rollInd++){
 
-      if(!((pitchInd==4 || pitchInd==1) && rollInd==5)){
+      if(!((pitchInd==5 && rollInd==5) || (pitchInd==10 && rollInd==10))){
   	continue;
       }
       
@@ -55,7 +56,7 @@ void drawLookForPitchRollOffsetPlots(){
 
       // TString command = TString::Format("deltaPhiDegs[%d][%d]:zoomPhiDeg>>", pitchInd, rollInd);
       // command += profName;
-      TString command = TString::Format("deltaThetaDegs[%d][%d]:zoomPhiDeg>>", pitchInd, rollInd);
+      TString command = TString::Format("deltaPhiDegs[%d][%d]:zoomPhiDeg>>", pitchInd, rollInd);
       command += profName;
 
       std::cout << pitchInd << "\t" << rollInd << "\t" << command.Data() << std::endl;
@@ -81,13 +82,11 @@ void drawLookForPitchRollOffsetPlots(){
   l->Draw();
   profs[0]->SetTitle("Pitch roll offsets");
 
-  return ;
 
 
 
-
-  TH2D* hThetaRmsVals = new TH2D("hThetaRmsVals",
-				 "RMS of #delta#theta distributions; Pitch offset (degrees); Roll offset (degrees); RMS (Degrees)",
+  TH2D* hPhiRmsVals = new TH2D("hPhiRmsVals",
+				 "RMS of #delta#phi distributions; Pitch offset (degrees); Roll offset (degrees); RMS (Degrees)",
 				 numPitches, pitchOffsets[0], pitchOffsets[numPitches-1]+deltaPitch,
 				 numRolls, rollOffsets[0], rollOffsets[numRolls-1]+deltaRoll);
 
@@ -95,6 +94,11 @@ void drawLookForPitchRollOffsetPlots(){
   for(Int_t pitchInd=0; pitchInd < numPitches; pitchInd++){
     for(Int_t rollInd=0; rollInd < numRolls; rollInd++){
 
+      if(!((pitchInd==5 && rollInd==5) || (pitchInd==10 && rollInd==10))){
+  	continue;
+      }
+
+      
       TString histName = TString::Format("h_%d_%d", pitchInd, rollInd);
       TString histTitle = TString::Format("Pitch %2.1lf, Roll %2.1lf",
 					  pitchOffsets[pitchInd], rollOffsets[rollInd]);
@@ -103,18 +107,18 @@ void drawLookForPitchRollOffsetPlots(){
 
       // TString command = TString::Format("deltaPhiDegs[%d][%d]:zoomPhiDeg>>", pitchInd, rollInd);
       // command += histName;
-      TString command = TString::Format("deltaThetaDegs[%d][%d]>>", pitchInd, rollInd);
+      TString command = TString::Format("deltaPhiDegs[%d][%d]>>", pitchInd, rollInd);
       command += histName;
 
       TString cuts = TString::Format("TMath::Abs(deltaPhiDegs[%d][%d]) < 10", pitchInd, rollInd);
       pitchRollChain->Draw(command, cuts, "goff");
 
 
-      hThetaRmsVals->SetBinContent(pitchInd+1, rollInd+1, h->GetRMS());
+      hPhiRmsVals->SetBinContent(pitchInd+1, rollInd+1, h->GetRMS());
       p++;
     }
   }
-  hThetaRmsVals->Draw("colz");
+  hPhiRmsVals->Draw("colz");
   
 
 }
